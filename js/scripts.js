9ab153345b7ff9a9403d1d639b0d7c10969060fa
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-10-02 11:18:39 -0700

    better version than prototype

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,16 @@
 // var allImg = ['img/magic8ball_1.png', 'img/magic8ball_2.png', 'img/magic8ball_3.png', 'img/magic8ball_4.png', 'img/magic8ball_5.png', 'img/magic8ball_6.png', 'img/magic8ball_7.png', 'img/magic8ball_8.png', 'img/magic8ball_9.png', 'img/magic8ball_10.png', 'img/magic8ball_11.png', 'img/magic8ball_12.png', 'img/magic8ball_13.png', 'img/magic8ball_14.png', 'img/magic8ball_15.png', 'img/magic8ball_16.png', 'img/magic8ball_17.png', 'img/magic8ball_18.png', 'img/magic8ball_19.png', 'img/magic8ball_20.png'];
 // console.log(allImg);
 
+// function randomPic() {
+//     var magicBallImg = new Array();
+//     magicBallImg[0] = "img/magic8ball_1.png";
+//     magicBallImg[1] = "img/magic8ball_2.png";
+//     magicBallImg[2] = "img/magic8ball_3.png";
+//     var random = Math.floor(Math.random() * magicBallImg.length);
+//     document.getElementById("randomImg").src = magicBallImg[random];
+//   }
+
 function randomPic() {
-    var magicBallImg = new Array();
-    magicBallImg[0] = "img/magic8ball_1.png";
-    magicBallImg[1] = "img/magic8ball_2.png";
-    magicBallImg[2] = "img/magic8ball_3.png";
-    var random = Math.floor(Math.random() * magicBallImg.length);
-    document.getElementById("randomImg").src = magicBallImg[random];
-  }
+  var randomNum = Math.floor((Math.random()*20)+1);
+  $('#randomImg').attr('src', 'img/magic8ball_' + randomNum + '.png');
+}
