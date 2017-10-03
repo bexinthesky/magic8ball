e40b5464c3375ea7574b33ffc6eba7825fa5a496
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-10-03 13:12:53 -0700

    got rid of extra event listener function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
 // target your button and assign an ask function that outputs a random image to your page
 
-
-// Click Event Listener with function(event)
-document.getElementById('askBtn').addEventListener("click", function(event) {
-  console.log(this, 'event listener');
-});
-
 //Function that uses Math.floor and Math.random to generate a random number and then is used to change the name of the image source being shown-and therefore the actual image shown
 function randomPic(event) {
   var randomNum = Math.floor((Math.random()*20)+1);
